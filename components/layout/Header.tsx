eb379fe74d59890f4b67a4c531b2f99419944ed0
pixellateddev <pixellateddev@gmail.com>
2022-03-09 17:41:00 +0530

    added header

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,10 +1,27 @@
+import Link from "next/link"
 import { FC } from "react"
 
 const Header: FC = () => {
     return (
-        <div>
-            Header
-        </div>
+        <header className="header">
+            <nav className="nav container">
+                <Link href="/">
+                    <a><img src="assets/images/logo.svg" alt="logo" className="logo"/></a>
+                </Link>
+                <ul className="nav-links">
+                    <li>
+                        <Link href="/blog">
+                            <a className="nav-link">Blog</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/about">
+                            <a className="nav-link">About</a>
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
     )
 }
 
