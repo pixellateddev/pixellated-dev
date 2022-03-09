c7bc3ba930a3daf703b56056999efa49fa40397f
pixellateddev <pixellateddev@gmail.com>
2022-03-09 16:57:12 +0530

    setup sass

---
+++ components/layout/index.tsx
@@ -0,0 +1,15 @@
+import { FC } from "react"
+import Footer from "./Footer"
+import Header from "./Header"
+
+const Layout: FC = ({children}) => {
+    return (
+        <div>
+            <Header />
+            {children}
+            <Footer />
+        </div>
+    )
+}
+
+export default Layout
